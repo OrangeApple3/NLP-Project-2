d9dc266656bb4080075102e71e02717dc7fee36f
Edwin Ma <em577@cornell.edu>
2016-10-03 19:44:52 -0400

    Edited Baseline to Include Keelan's Idea

--- baseline.py
+++ baseline.py
@@ -2,8 +2,7 @@ import os
 import sys
 
 
-# python3 baseline.py <file_type>  <competition_type> 
-# file_type is public or private
+# python3 baseline.py <competition_type> 
 # competition_type is span or sentence
 
 
@@ -11,20 +10,35 @@ def build_wordDict():
     word_dictionary={}
     for fn in os.listdir("train/"):
         with open("train/" + fn) as train_file:
+            state = "O" 
             for lines in train_file:
                 lines = lines.rstrip()
-                if lines != '':
-                    word,pos,tag=lines.split()
-                    if tag != "_":
+                if state == "O" and lines != '':
+                    state = "B"
+                    word, pos, tag = lines.split()
+                    word_dictionary[word] = \
+                        word_dictionary.get(word,set()).union({(pos, state)})
+                elif state == "B":
+                    if lines == '':
+                        state = "O"
+                    else:
+                        state = "I"
+                        word, pos, tag = lines.split()
                         word_dictionary[word] = \
-                            word_dictionary.get(word,set()).union({pos})
+                            word_dictionary.get(word,set()).union({(pos, state)})
+                elif state == "I":
+                    if lines == '':
+                        state = "O"
+                    else:
+                        word, pos, tag = lines.split()
+                        word_dictionary[word] = \
+                            word_dictionary.get(word,set()).union({(pos, state)})    
     return(word_dictionary)
 
 def test_annotate(wordDict, file_type, competition_type):
     if competition_type == "span":
         token_id = -1
         span_list = []
-        print("Type,Spans")
         for fn in os.listdir("test-{}/".format(file_type)):
             with open("test-{}/{}".format(file_type, fn)) as test_file:
                 state = "O"
@@ -34,7 +48,7 @@ def test_annotate(wordDict, file_type, competition_type):
                     token_id = token_id + 1 if lines != '' else token_id
                     if state == "O" and lines != '':
                         word, pos = lines.split()
-                        if word in wordDict and pos in wordDict[word]:
+                        if word in wordDict and (pos,"B") in wordDict[word]:
                             beginning = token_id
                             state = "B"
                     elif state == "B":
@@ -44,7 +58,7 @@ def test_annotate(wordDict, file_type, competition_type):
                             state = "O"
                         else:
                             word, pos = lines.split()
-                            if word in wordDict and pos in wordDict[word]:
+                            if word in wordDict and (pos,"I") in wordDict[word]:
                                 state = "I"
                             else:
                                 span_list.append("{}-{}".format(beginning, 
@@ -58,7 +72,7 @@ def test_annotate(wordDict, file_type, competition_type):
                         else:
                             word, pos = lines.split()
                             if (word not in wordDict) or \
-                               (pos not in wordDict[word]):
+                               ((pos,state) not in wordDict[word]):
                                 span_list.append("{}-{}".format(beginning, 
                                                                 token_id-1))
                                 state = "O"
@@ -70,7 +84,6 @@ def test_annotate(wordDict, file_type, competition_type):
         print("CUE-{},".format(file_type) + " ".join(span_list))
 
     elif competition_type == "sentence":
-        print("Type,Indices")
         sentence_id = -1
         sentence_list = []
         for fn in os.listdir("test-{}/".format(file_type)):
@@ -83,7 +96,7 @@ def test_annotate(wordDict, file_type, competition_type):
                         word, pos = lines.split()
                         if (len(sentence_list) == 0 or 
                             sentence_list[-1] != str(sentence_id)) and \
-                            (word in wordDict and pos in wordDict[word]):
+                            (word in wordDict and (pos,"B") in wordDict[word]):
                                 sentence_list.append(str(sentence_id))
                         state = "I"
                     elif state == "I":
@@ -92,15 +105,21 @@ def test_annotate(wordDict, file_type, competition_type):
                         else:
                             if (len(sentence_list) == 0 or 
                                 sentence_list[-1] != str(sentence_id)) and \
-                                (word in wordDict and pos in wordDict[word]):
+                                (word in wordDict and (pos,"B") in wordDict[word]):
                                     sentence_list.append(str(sentence_id))
         print("SENTENCE-{},".format(file_type) + " ".join(sentence_list))
 
 
-def main(wordDict, file_type, competition_type):
-    test_annotate(wordDict, file_type, competition_type)
-
+def main(wordDict, competition_type):
+    if competition_type == "span":
+        print("Type,Spans")
+        test_annotate(wordDict, "public", competition_type)
+        test_annotate(wordDict, "private", competition_type)
+    elif competition_type == "sentence":
+        print("Type,Indices")
+        test_annotate(wordDict, "public", competition_type)
+        test_annotate(wordDict, "private", competition_type)
     
 
 if __name__ == '__main__':
-  main(build_wordDict(), sys.argv[1], sys.argv[2])
+  main(build_wordDict(), sys.argv[1])
