fcf03ba9fcf14114e2a756bc0469eede03efa9b3
Edwin Ma <em577@cornell.edu>
2016-10-15 17:46:47 -0400

    Edited Viterbi

--- viterbi.py
+++ viterbi.py
@@ -4,10 +4,11 @@ import os
 
 # Returns highest (probability_of_tag_sequence, tag_sequence)
 def viterbi(emission_prob, transition_prob, word_POS_list):
-    BIO = ["B": 0, "I": 1, "O": 2]
+    BIO = {"B": 0, "I": 1, "O": 2}
 
     max_node_prob = []
-    max_node_prob.extend([(0,[]) for _ in range(3)] for _ in range(word_POS_list))
+    max_node_prob.extend([(0,[]) for _ in range(3)] 
+                         for _ in range(word_POS_list))
     
     for curr_BIO in BIO:
         max_node_prob[0][BIO[curr_BIO]] = (
@@ -27,7 +28,7 @@ def viterbi(emission_prob, transition_prob, word_POS_list):
                     * transition_prob[(curr_BIO, max_tag_tuple[1])],
                     # Appending most probable BIO tag
                     max_node_prob[curr_word-1][BIO[max_tag_tuple[1]]][1]
-                    + [max_tag_tuple[0]]
+                    + [curr_BIO]
             )
     return(max_node_prob[-1])
 
