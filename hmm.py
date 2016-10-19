2fcd00ed741b2ee9dfa4a59c2519d0f0195c4878
krc78 <krc78@cornell.edu>
2016-10-19 15:47:53 -0400

    viterbi fixes

--- hmm.py
+++ hmm.py
@@ -1,38 +1,45 @@
 import sys
 import os
 from collections import Counter
+from sets import Set
 from preprocessing import has_cue
 
 
 def compute_emission_probabilities(smoothed=False):
     """
     Computes the emission probabilities over the training data.
-    Emission prob. key/value example: ("table", "<B-CUE") : 0.015
+    Emission prob. key/value example: ("table", "<B-CUE") : 0.015.
+    Returns the emission probs dict and the word POS list
     """
     emission_counts = Counter()
     emission_probabilities = dict()
     cue_counts = Counter()
+    word_pos_list = []
     for file in os.listdir(os.getcwd() + "/train_preprocessed"):
         data = open(os.getcwd() + "/train_preprocessed/{}".format(file), 'r+')
         for line in data:
             line = line.strip("\n")
             cue = has_cue(line)
+            word = line.split("\t")[0].lower()
             if cue:
                 cue_counts[cue] += 1
                 # Emission count format example : {('students', '<B-CUE>'):3}
-                emission_counts[(line.split("\t")[0].lower(), cue)] += 1
+                emission_counts[(word, cue)] += 1
+                word_pos_list.append(word)
             # NULL state has no emission probabilities
             elif "NULL" not in line:
                 cue_counts["<O>"] += 1
-                emission_counts[(line.split("\t")[0].lower(), "<O>")] += 1
+                emission_counts[(word, "<O>")] += 1
+                word_pos_list.append(word)
     for word, tag in emission_counts.iterkeys():
         emission_probabilities[(word, tag)] = float(emission_counts[(word, tag)])/float(cue_counts[tag])
-    return emission_probabilities
+    return emission_probabilities, Set(word_pos_list)
 
 
 def test_emissions():
     # Sanity checks
-    emission_probs = compute_emission_probabilities()
+    emission_probs, word_pos_list = compute_emission_probabilities()
+    print "Total number of distinct words: {}".format(len(word_pos_list))
     sorted_probs = sorted(emission_probs, key=emission_probs.get, reverse=True)
 
     b_cue_probs = [pair for pair in sorted_probs if pair[1] == '<B-CUE>']
--- viterbi.py
+++ viterbi.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from hmm import compute_emission_probabilities
+from transition import compute_transition_probabilities
 
 # We do not make an explicit assumption on whether to keep zero probabilities in
 # the dictionary or not. 
@@ -12,7 +14,7 @@ def viterbi(emission_prob, transition_prob, word_POS_list):
 
     max_node_prob = []
     max_node_prob.extend([(0,[]) for _ in range(3)] 
-                         for _ in range(word_POS_list))
+                         for _ in range(len(word_POS_list)))
     
     for curr_BIO in BIO:
         e_prob = emission_prob.get((word_POS_list[0], curr_BIO),0)
@@ -21,23 +23,26 @@ def viterbi(emission_prob, transition_prob, word_POS_list):
 
     for curr_word in range(1,len(word_POS_list)):
         for curr_BIO in BIO:
-            max_tag_tuple = max(lambda t: transition_prob[t] 
-                                if t[0] == curr_BIO else -1, transition_prob)
+            max_tag_tuple = max(filter(lambda t: transition_prob[t]
+                                       if t[0] == curr_BIO else -1, transition_prob))
             e_prob = emission_prob.get((word_POS_list[curr_word], curr_BIO),0)
             t_prob = transition_prob.get((curr_BIO, max_tag_tuple[1]),0)
-
-            max_node_prob[curr_word][BIO[curr_BIO]] = (
-                    max_node_prob[curr_word-1][BIO[curr_BIO]][0] 
-                    *  e_prob * t_prob,
-                    # Appending most probable BIO tag
-                    max_node_prob[curr_word-1][BIO[max_tag_tuple[1]]][1]
-                    + [curr_BIO]
-            )
+            
+            if max_tag_tuple[1] != 'NULL':
+                max_node_prob[curr_word][BIO[curr_BIO]] = (
+                        max_node_prob[curr_word-1][BIO[curr_BIO]][0] 
+                        *  e_prob * t_prob,
+                        # Appending most probable BIO tag
+                        max_node_prob[curr_word-1][BIO[max_tag_tuple[1]]][1]
+                        + [curr_BIO]
+                )
     return(max_node_prob[-1])
 
 
 def main():
-    pass
+    emission_probs, word_pos_list = compute_emission_probabilities()
+    trans_probs = compute_transition_probabilities()
+    print viterbi(emission_probs, trans_probs, list(word_pos_list))
 
 
 if __name__ == "__main__":
